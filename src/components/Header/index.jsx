4c561b5c172bb91a4ba5b12580f3f5323d112727
Kerne1bug <i@pfedulov.ru>
2023-08-09 16:20:52 +0300

    done rev 0.1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
 import styles from './styles.module.css';
 // import BCKGRND from './assets/bckground.png';
 import FeedBackForm from '../ConnectWithUsForm';
+import Message from './assets/message.svg';
+import Bckgrnd from './assets/bckground.png';
 
 function Header() {
 	return (
-		<div className={styles.layout}>
+		<div className={styles.header}>
+			<img className={styles.backImg} src={Bckgrnd} alt="Сына-корзина" />
+			<img className={styles.formIcon} src={Message} alt="Вам пришло новое сообщение" />
+			<div className={styles.semiCircle} />
 			<FeedBackForm />
 		</div>
 	);
